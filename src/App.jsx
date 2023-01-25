26cd8fe394483d93113047dc3a006b789251ffcb
tejas-gk <tejgankri@outlook.com>
2023-01-25 18:26:01 +0530

    changed title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ import ProfileCard from './pages/ProfileCard'
 import Advice from './pages/Advice'
 import PasswordGenerator from './pages/PasswordGenerator'
 import Dictionary from './components/UIProjects/dictionary/index'
-import Pomodoro from './components/UIProjects/pomodoro/index'
 import GameOfLife from './components/UIProjects/game-of-life/index'
 import News from './pages/News'
 export default function App() {
@@ -41,7 +40,6 @@ export default function App() {
             <Route path='advice' element={<Advice />} />
             <Route path='dictionary' element={<Dictionary />} />
             <Route path='password-generator' element={<PasswordGenerator />} />
-            <Route path='pomodoro' element={<Pomodoro />} />
             <Route path='game-of-life' element={<GameOfLife />} />
             <Route path='news' element={<News />} />
           <Route path='*' element={<div>404</div>} />
